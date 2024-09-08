560c9d6fc06720916b4d0fb0adf7a07542022bb9
Hpandey10 <101harsh.pandey101@gmail.com>
2024-09-09 01:51:22 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import myImage1 from "./images/Harsh.jpg"
+import myImage2 from "./images/kunwar.jpg"
+import myImage3 from "./images/Nikhil.jpg"
+import myImage4 from "./images/Mritunjay.jpg"
+import myImage5 from "./images/manjeet.jpg"
+const reviews = [
+  {
+    id: 1,
+    name: "Harsh Pandey",
+    job: "Software Engineer",
+    image: myImage1,
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Kunwar",
+    job: "Graphic Designer",
+    image: myImage2,
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Nikhil kumar",
+    job: "Marketing Manager",
+    image: myImage3,
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Mritunjay Rai",
+    job: "Content Strategist",
+    image: myImage4,
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Manjeet Mishra",
+    job: "Data Analyst",
+    image: myImage5,
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
